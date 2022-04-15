38e744be992d931c29ce6e4bfc1ba440f0017763
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2022-04-15 22:34:37 +0100

    create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,7 @@
+#creates a file named holberton in /tmp using Puppet
+file { '/tmp/school':
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
+}
