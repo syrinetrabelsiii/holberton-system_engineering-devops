8e0393cb804d3ef70438b102d4e8604472a191cd
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2022-05-07 13:10:16 +0100

    pupet config

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+#Creates Sets up client SSH configuration file
+file_line { 'ssh_config':
+    ensure   => present,
+    path     => '/etc/ssh/ssh_config',
+    line     => 'PasswordAuthentication no',
+    multiple => 'true'
+}
+file_line { 'ssh_config_2':
+    ensure   => present,
+    path     => '/etc/ssh/ssh_config',
+    line     => 'IdentityFile ~/.ssh/school',
+    multiple => 'true'
+}
