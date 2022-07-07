8ea6fe8b201ba38c180da0f78b207cb2b437d125
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2022-07-07 16:01:50 +0100

    install package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # installing puppet-lint using Puppet
 
-package { 'puppet-lint':
-    ensure   => '2.5.0',
-    provider => 'gem',
+package { 'flask':
+    ensure   => '2.1.1',
+    provider => 'pip',
 }
